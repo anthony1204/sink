41c2bcd7b5e43e1a4312a074f6604ac59a3d626c
anthony1204 <78404321+anthony1204@users.noreply.github.com>
2024-10-19 22:00:02 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
-  title: 'AShorten',
-  description: 'omg link shortener',
+  title: 'Sink (Anthony Edition)',
+  description: 'Link shortener for anthony only because why not.',
   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNI_mzHas4gtrC5xJraZEoE46qn0jk92Tn4g&s',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
