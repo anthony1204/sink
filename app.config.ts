2eb634e6b275bf283103979c6096280ca377ae85
anthony1204 <78404321+anthony1204@users.noreply.github.com>
2024-10-19 21:56:10 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'AShorten',
+  description: 'omg link shortener',
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNI_mzHas4gtrC5xJraZEoE46qn0jk92Tn4g&s',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
